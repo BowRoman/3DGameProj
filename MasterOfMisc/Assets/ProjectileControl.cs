0dfb52121f7784bb8c040f225cb436f197eaf804
Jake Roman-Barnes <bowenrombar@gmail.com>
2016-11-25 01:58:52 -0800

    Added Projectile firing. (Toward mouse location)

--- MasterOfMisc/Assets/ProjectileControl.cs
+++ MasterOfMisc/Assets/ProjectileControl.cs
@@ -4,15 +4,32 @@ using System.Collections;
 public class ProjectileControl : MonoBehaviour {
 
     [SerializeField]
-    float speed = 10.0f;
+    public float speed = 10.0f;
+
+    float lifespan = 5.0f;
 
 	void Start ()
     {
-	
-	}
+        print("Projectile created.");
+    }
 	
 	void Update ()
     {
-
+        LifeClock();
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+
+    // Destroys object when lifespan is exceeded.
+    void LifeClock()
+    {
+        lifespan -= Time.deltaTime;
+        if (lifespan <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- MasterOfMisc/Assets/WeaponControl.cs
+++ MasterOfMisc/Assets/WeaponControl.cs
@@ -2,20 +2,36 @@
 using System.Collections;
 
 public class WeaponControl : MonoBehaviour {
-    
-	void Start () {
+
+    [SerializeField]
+    GameObject projectile;
+
+    Vector3 lookPos;
+
+    void Start () {
 	
 	}
 	
 	void Update ()
     {
         RotateSpriteToCursor();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireBullet();
+        }
+    }
+
+    public void FireBullet()
+    {
+        print("Fire called");
+        GameObject projectileClone = (Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, 1)))) as GameObject;
+        projectileClone.GetComponent<Rigidbody2D>().velocity = lookPos * projectileClone.GetComponent<ProjectileControl>().speed;
     }
 
     void RotateSpriteToCursor()
     {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = Camera.main.ScreenToWorldPoint(mousePos);
         lookPos = lookPos - transform.position;
         float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
